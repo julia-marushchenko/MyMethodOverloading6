ea979871bfc5e6c232d34c424e54a194b65e24e8
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-07-16 02:35:33 +0200

    Add files via upload
    Java program to demonstrate methods overloading with static and non-static modifiers

---
+++ src/com/methods/Main.java
@@ -0,0 +1,33 @@
+// Java program to demonstrate methods overloading with static and non-static modifiers
+
+package com.methods;
+
+// Main class
+public class Main {
+
+    // Method get to sum two integers
+    public static int add(int number, int number1){ // Compiler exception
+        int sum = number + number1;
+        return sum;
+    }
+
+    // Method get to sum three integers
+    public int add(int number, int number1, int number2){
+        int sum = number + number1;
+        return sum;
+    }
+
+    // Main method to run java program
+    public static void main(String[] args) {
+
+        // Creating Main instance
+        Main m = new Main();
+
+        // Calling method add() with two arguments
+        System.out.println(Main.add(3, 5));
+
+        // Calling method add() with three arguments
+        System.out.println(m.add(3, 5, 8)); // Calls wrong method with two arguments
+
+    }
+}
